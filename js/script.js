06a0cb60a6c4043f6293de40934685941009fba5
Tyler Furgalack <104029862+tylerfurgalack@users.noreply.github.com>
2024-06-08 00:30:41 -0400

    about-img added, trying to fix nav anchors

--- js/script.js
+++ js/script.js
@@ -40,50 +40,50 @@ const obs = new IntersectionObserver(
 
 obs.observe(sectionHeroEl);
 
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
+// ///////////////////////////////////////////////////////////
+// // Smooth scrolling animation
 
-const allLinks = document.querySelectorAll('a:link');
+// const allLinks = document.querySelectorAll('a:link');
 
-allLinks.forEach(function (link) {
-  link.addEventListener('click', function (e) {
-    e.preventDefault();
-    const href = link.getAttribute('href');
+// allLinks.forEach(function (link) {
+//   link.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const href = link.getAttribute('href');
 
-    // Scroll back to top
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
+//     // Scroll back to top
+//     if (href === '#')
+//       window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth',
+//       });
 
-    // Scroll to other links
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: 'smooth' });
-    }
+//     // Scroll to other links
+//     if (href !== '#' && href.startsWith('#')) {
+//       const sectionEl = document.querySelector(href);
+//       sectionEl.scrollIntoView({ behavior: 'smooth' });
+//     }
 
-    // Close mobile naviagtion
-    if (link.classList.contains('main-nav-link'))
-      headerEl.classList.toggle('nav-open');
-  });
-});
+//     // Close mobile naviagtion
+//     if (link.classList.contains('main-nav-link'))
+//       headerEl.classList.toggle('nav-open');
+//   });
+// });
 
-///////////////////////////////////////////////////////////
-// Fixing flexbox gap property missing in some Safari versions
-function checkFlexGap() {
-  var flex = document.createElement('div');
-  flex.style.display = 'flex';
-  flex.style.flexDirection = 'column';
-  flex.style.rowGap = '1px';
+// ///////////////////////////////////////////////////////////
+// // Fixing flexbox gap property missing in some Safari versions
+// function checkFlexGap() {
+//   var flex = document.createElement('div');
+//   flex.style.display = 'flex';
+//   flex.style.flexDirection = 'column';
+//   flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement('div'));
-  flex.appendChild(document.createElement('div'));
+//   flex.appendChild(document.createElement('div'));
+//   flex.appendChild(document.createElement('div'));
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
+//   document.body.appendChild(flex);
+//   var isSupported = flex.scrollHeight === 1;
+//   flex.parentNode.removeChild(flex);
 
-  if (!isSupported) document.body.classList.add('no-flexbox-gap');
-}
-checkFlexGap();
+//   if (!isSupported) document.body.classList.add('no-flexbox-gap');
+// }
+// checkFlexGap();
